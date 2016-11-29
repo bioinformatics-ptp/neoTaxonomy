aff23ab90e17211101817e4655baed1705f23a65
Paolo Cozzi <paolo.cozzi@ptp.it>
2016-11-29 12:51:31 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 12:48:33 2016
+
+@author: Paolo Cozzi <paolo.cozzi@ptp.it>
+"""
+
+import io
+import re
+
+# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-import sys, os
+# To use a consistent encoding
+from codecs import open
+from os import path
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+here = path.abspath(path.dirname(__file__))
 
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    README = f.read()
 
-version = '0.1.0'
+with open(path.join(here, 'NEWS.txt'), encoding='utf-8') as f:
+    NEWS = f.read()
+
+# define function to parse versions
+# https://packaging.python.org/en/latest/single_source_version.html
+def read(*names, **kwargs):
+    with io.open(
+        path.join(path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+    
+# package version
+version = find_version('src/neotaxonomy', "__init__.py")
 
 install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    "py2neo",
 ]
 
 
-setup(name='neotaxonomy',
+setup(
+    name='neotaxonomy',
+    
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
     version=version,
+    
     description="A neo4j API for NCBI taxonomy database",
     long_description=README + '\n\n' + NEWS,
-    classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    ],
-    keywords='NCBI taxonomy neo4j',
+    
+    # The project's main homepage.
+    url='',
+    
+    # Author details
     author='Paolo Cozzi',
     author_email='paolo.cozzi@ptp.it',
-    url='',
+    
+    # Provide the type of license you are using
     license='GPLv3',
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},include_package_data=True,
-    zip_safe=False,
+    
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 1 - Planning",
+        
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Topic :: Database :: Front-Ends",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        # TODO: add more python versions
+    ],
+    
+    # List keywords that describe your project.
+    keywords='NCBI taxonomy neo4j',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # Use the exclude keyword argument to omit packages that are not intended 
+    # to be released and installed    
+    packages=find_packages('src', exclude=['contrib', 'docs', 'tests*']),
+    package_dir = {'': 'src'},
+    include_package_data=True,
+    
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
     install_requires=install_requires,
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+#    package_data={
+#        'sample': ['package_data.dat'],
+#    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+#    data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts':
             ['neotaxonomy=neotaxonomy:main']
-    }
+    },
 )
--- src/neotaxonomy/__init__.py
+++ src/neotaxonomy/__init__.py
@@ -1,4 +1,24 @@
-# Example package with a console entry point
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 12:47:56 2016
 
+@author: Paolo Cozzi <paolo.cozzi@ptp.it>
+"""
+
+__author__ = "Paolo Cozzi"
+__copyright__ = "Copyright 2016, Paolo Cozzi"
+__credits__ = ["Paolo Cozzi"]
+__license__ = "GNU GPLv3"
+__version__ = "0.1.0"
+__maintainer__ = "Paolo Cozzi"
+__email__ = "paolo.cozzi@ptp.it"
+__status__ = "alfa"
+
+
+# All libraries imported with import *
+__all__ = []
+
+
+# Example package with a console entry point
 def main():
     print "Hello World"
